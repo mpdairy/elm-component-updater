4ef1fba0fe2302b898c4718f74e0a6ed772cf6cd
Matt Parker <mpdairy@gmail.com>
2016-11-04 20:04:56 -0400

    Many

--- examples/src/Component/Many.elm
+++ examples/src/Component/Many.elm
@@ -1,6 +1,9 @@
 module Component.Many exposing (Msg (..), Model, initModel, update, subscriptions)
 
+import Html exposing (Html)
+import Html.App as Html
 import Dict exposing (Dict)
+import List
 
 import Updater exposing (converter, Updater, Converter, Interface, toCmd, noReaction)
 
@@ -10,10 +13,14 @@ type alias Model cModel cMsg =
     , newID : Int
     , converter : (Int -> Converter (Msg cModel cMsg) cMsg)
     , objUpdate : ( cMsg -> cModel -> ( cModel, Cmd cMsg ))
-    , objSubs : ( cModel -> Sub cMsg )}
+    , objSubs : ( cModel -> Sub cMsg )
+    , viewAll : ( Int -> cModel -> ( Html cMsg -> Html (Msg cModel cMsg) )
+                -> Maybe (Html (Msg cModel cMsg)) )
+              -> List (Html (Msg cModel cMsg)) }
 
 type Msg cModel cMsg = Add (cModel, Cmd cMsg)
                      | Delete Int
+                     | Passing Int cMsg cModel
                      | UpdaterMsg (Updater (Model cModel cMsg) (Msg cModel cMsg))
 
 objectC : (cMsg -> cModel -> (cModel, Cmd cMsg)) -> Int
@@ -22,11 +29,25 @@ objectC objUpdate n =
     converter
         UpdaterMsg
         { get = (\ model -> Dict.get n model.objects)
-        , set = (\ object model -> { model | objects = Dict.insert n object model.objects } )
+        , set = (\ object model ->
+                     let newObjects = Dict.insert n object model.objects in
+                     { model | objects = newObjects
+                     , viewAll = mapper objUpdate newObjects} )
         , update = objUpdate
-        , react = noReaction }
+        , react = (\ cMsg cModel model -> model ! [ toCmd <| Passing n cMsg cModel ] ) }
 --
+mapper : ( cMsg -> cModel -> ( cModel, Cmd cMsg ))
+       -> Dict Int cModel
+       -> ( Int -> cModel -> ( Html cMsg -> Html (Msg cModel cMsg) )
+          -> Maybe ( Html ( Msg cModel cMsg ) ) )
+       -> List (Html (Msg cModel cMsg))
+mapper objUpdate objects f =
+    List.filterMap 
+        (\ (id, cModel) ->
+             f id cModel (Html.map <| objectC objUpdate id)) <|
+        Dict.toList objects
 
+--
 initModel : ( cMsg -> cModel -> ( cModel, Cmd cMsg ) )
           -> ( cModel -> Sub cMsg )
           -> Model cModel cMsg
@@ -34,7 +55,8 @@ initModel objUpdate objSubs = { objects = Dict.empty
                               , newID = 0
                               , converter = objectC objUpdate
                               , objUpdate = objUpdate
-                              , objSubs = objSubs }
+                              , objSubs = objSubs
+                              , viewAll = mapper objUpdate Dict.empty }
 
 -- UPDATE
 update : Msg cModel cMsg -> Model cModel cMsg
@@ -42,11 +64,19 @@ update : Msg cModel cMsg -> Model cModel cMsg
 update msg model =
   case msg of
       Add (objectModel, objectCmd) ->
-          { model | objects = Dict.insert model.newID objectModel model.objects
+          let newObjects = Dict.insert model.newID objectModel model.objects in
+          { model | objects = newObjects
+          , viewAll = mapper model.objUpdate newObjects
           , newID = model.newID + 1 }
           ! [ Cmd.map (objectC model.objUpdate model.newID) <| objectCmd ]
 
-      Delete id -> { model | objects = Dict.remove id model.objects } ! []
+      Delete id ->
+          let newObjects = Dict.remove id model.objects in
+          { model | objects = newObjects
+          , viewAll = mapper model.objUpdate newObjects } ! []
+
+      --Passes object messages along to parent
+      Passing id cMsg cModel -> model ! []
 
       UpdaterMsg u -> u model
 
--- examples/src/Component/SuperTimer.elm
+++ examples/src/Component/SuperTimer.elm
@@ -1,40 +1,49 @@
-module Component.ManualTimerCluster exposing (Msg (..), Model, init, update, view)
+module Component.SuperTimer exposing (Msg (..), Model, init, update, view, subscriptions)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.App as Html
 import Html.Events exposing ( onClick )
 --import Time exposing (Time, every, second)
 --import Random
---import Dict exposing (Dict)
+import Dict exposing (Dict)
 import Component.TaskTimer as TaskTimer
 import Component.Many as Many
 --import Component.SuperBuzzer as Buzzer
 
 import Updater exposing (converter, Updater, Converter, Interface, toCmd, noReaction)
 
-type alias TimersModel = Many.Model TaskTimer.Model TaskTimer.Msg
 
-type alias TimersMsg = Many.Msg TaskTimer.Model TaskTimer.Msg
+type alias Model = { timers : Many.Model TaskTimer.Model TaskTimer.Msg
+                   , message : String }
 
-
-type alias Model = { timers : TimersModel }
 
 type Msg = NoOp
          | UpdaterMsg (Updater Model Msg)
 
 
-timersC : Converter Msg TimersMsg
+timersC : Converter Msg (Many.Msg TaskTimer.Model TaskTimer.Msg)
 timersC = converter
            UpdaterMsg
            { get = Just << .timers
            , set = (\ cm model -> { model | timers = cm } )
            , update = Many.update
-           , react = noReaction }
+           , react = (\ msg _ model ->
+                          case msg of
+                              Many.Passing id cMsg cModel ->
+                                  case cMsg of
+                                      TaskTimer.BuzzMessage name description ->
+                                          { model | message = name ++ " : " ++ description }
+                                          ! []
+
+                                      _ -> model ! []
+
+                              _ -> model ! [] ) }
 
 --
 
 init : (Model, Cmd Msg)
-init = { timers = Many.initModel TaskTimer.update TaskTimer.subscriptions }
+init = { timers = Many.initModel TaskTimer.update TaskTimer.subscriptions
+       , message = ""}
     ! [ ]
 
 -- UPDATE
@@ -54,32 +63,20 @@ subscriptions model =
 -- CSS can be applied via class names or inline style attrib
 view : Model -> Html Msg
 view model =
-    div []
-        [ Html.map timersC <| viewTimers model.timers ]
-
-viewTimers : TimersModel -> Html TimersMsg
-viewTimers timers =
-    div [ class "timers" ]
-        [ div [ style [("height", "420px")]] <|
-
-              timers.viewAll
-              (\ id timer conv -> Just <|
-                   div [ style [ ("width", "215px")
-                               , ("float", "left")
-                               , ("height", "320px") ] ]
-                   [ conv <| TaskTimer.view timer
-                   , button [ onClick <| Many.Delete id ] [ text "Delete" ] ])
-
-        , div [] [ button [ onClick <| Many.Add TaskTimer.init ] [ text "Add Timer" ]]]
-
-
-{-
+  div [ ]
+      [ div [ style [("height", "420px")]] <|
             List.map (\ (id, timerModel) ->
                           deletableTimer id <|
                           Html.map (timersC << model.timers.converter id) <|
                           TaskTimer.view timerModel)
             (Dict.toList model.timers.objects)
--}
+
+      , div [] [
+             Html.map timersC <|
+             button [ onClick <| Many.Add TaskTimer.init ] [ text "Add Timer" ] ]
+      , h4 [] [ text model.message ]
+      ]
+
 
 deletableTimer : Int -> Html Msg -> Html Msg
 deletableTimer id html = div [ style [ ("width", "215px")
